d65ddc028f6bf31f026b8f10f52263bfe00bc597
ad0726 <ad0726@hotmail.fr>
2018-07-03 02:16:57 +0200

    - create and setup modif page - css - upload covers

--- header.php
+++ header.php
@@ -6,12 +6,22 @@ include('function.php');
 <html>
     <head>
         <title>DCT-ODL</title>
-        <link rel="stylesheet" href="style.css?1.0.2">
+        <link rel="stylesheet" href="assets/css/style.css?1.0.0">
+        <link rel="stylesheet" href="assets/css/general.css?1.0.0">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="script.js"></script>
+        <script type="text/javascript" src="assets/js/script.js"></script>
     <head>
     <body>
         <header>
-            <?php displayHeader(); ?>    
+            <div class="title_header">
+                <h1>DCTrad présente</h1>
+                <h2>L'ordre de lecture DC</h2>
+            </div>
+            <?php displayHeader(); ?>
         </header>
+<?php
+include("login.php");
+logout();
+    // print_r($_SERVER);
+?>
