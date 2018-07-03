b383e1ec1a2f2922324f53cf6c569f310244b84c
ad0726 <ad0726@hotmail.fr>
2018-07-03 11:09:41 +0200

    clean function

--- function.php
+++ function.php
@@ -36,94 +36,12 @@ function logout() {
 function displayLine($line) {
  echo "
     <tr class='line' id='".$line['id']."'>
-        <td class='nolog'>".$line['id'];
-        if (isset($_SESSION['pseudo']))
-        {
-            echo "<form action=\"#".$line['id']."\" method=\"post\">
-                <input type=\"hidden\" name=\"formfilled\" value=\"42\" />
-                <br /><input class='modify_form' type=\"number\" name=\"new_id\">";
-        }
-        echo "</td>
-        <td><img src=\"".$line['cover']."\" >";
-        if (isset($_SESSION['pseudo']))
-        {
-            echo "<br /><input class='modify_form' type=\"text\" name=\"new_cover\" placeholder=\"URL\">";
-        }
-        echo "</td>
-        <td><h3>".$line['arc']."</h3>";
-        if (isset($_SESSION['pseudo']))
-        {
-            echo "<br /><input class='modify_form' type=\"text\" name=\"new_title\" placeholder=\"Nouveau titre\">";
-        }
-        echo "</td>
-        <td><p>".nl2br($line['contenu'])."</p>";
-        if (isset($_SESSION['pseudo']))
-        {
-            echo "<textarea class='modify_form' type=\"text\" name=\"new_contenu\" placeholder=\"Nouveau contenu\"></textarea>";
-        }
-        echo "</td>
-        <td class='nolog'>";
-        if (isset($_SESSION['pseudo']))
-        {
-            button(1, 1, $line);
-        }
-    echo "</td>
+        <td class='nolog'>".$line['id']."</td>
+        <td><img src=\"".$line['cover']."\" ></td>
+        <td><h3>".$line['arc']."</h3></td>
+        <td><p>".nl2br($line['contenu'])."</p></td>
+        <td class='nolog'>
     </tr>";
-}
-
-function button($modify, $delete, $line) {
-    global $bdd;
-    if (!empty($modify)) {
-        echo "<button class='modify' type='button' ><i class=\"fas fa-wrench\"></i></button>";
-        if ($_SERVER['QUERY_STRING'] == $line['id']) {
-            echo "<input class='modify_form' type=\"submit\" value=\"Envoyer\">
-                </form>";
-                if (!empty($_REQUEST['new_id'])) {
-                    $bdd->exec('UPDATE rebirth SET id = \'-1\' WHERE id = \''.$line['id'].'\'');
-                    $bdd->query('UPDATE rebirth SET id = id - 1 WHERE id BETWEEN '.$line['id'].' AND '.$_REQUEST['new_id']);
-                    $bdd->exec('UPDATE rebirth SET id = \''.$_REQUEST['new_id'].'\' WHERE id = \'-1\'');
-                    $bdd->exec('ALTER TABLE rebirth ORDER BY id ASC');
-                } elseif (!empty($_REQUEST['new_id'])) {
-                    $bdd->exec('UPDATE rebirth SET id = \'-1\' WHERE id = '.$line['id']);
-                    $bdd->query('UPDATE rebirth SET id = id + 1 WHERE id BETWEEN '.$_REQUEST['new_id'].' AND '.$line['id']);
-                    $bdd->exec('UPDATE rebirth SET id = \''.$_REQUEST['new_id'].'\' WHERE id = \'-1\'');
-                    $bdd->exec('ALTER TABLE rebirth ORDER BY id ASC');
-                }
-                if (!empty($_REQUEST['new_cover'])) {
-                    $bdd->exec('UPDATE rebirth SET cover = \''.$_REQUEST['new_cover'].'\' WHERE id = \''.$line['id'].'\'');
-                }
-                if (!empty($_REQUEST['new_title'])) {
-                    $bdd->exec('UPDATE rebirth SET arc = \''.$_REQUEST['new_title'].'\' WHERE id = \''.$line['id'].'\'');
-                }
-                if (!empty($_REQUEST['new_contenu'])) {
-                    $bdd->exec('UPDATE rebirth SET contenu = \''.$_REQUEST['new_contenu'].'\' WHERE id = \''.$line['id'].'\'');
-                }
-                if (isset($_REQUEST['formfilled']) && $_REQUEST['formfilled'] == 42) {
-                    header('Location: '.$_SERVER['HTTP_HOST']);
-                }
-        }
-    }
-    if (!empty($delete)) {
-        echo "<button class='delete' type='button' ><a href=\"?delete".$line['id'];
-        echo"\"><i class=\"fas fa-trash-alt\"></i></a></button>";
-        if ($_SERVER['QUERY_STRING'] == "delete".$line['id']) {
-            echo "<form action=\"?delete".$line['id']."\" method=\"post\">
-            <input type=\"hidden\" name=\"delete\" value=\"0\">Sûr ?<br />
-            <input type=\"submit\" value=\"Oui\">
-            </form><form action=\"?delete".$line['id']."\" method=\"post\">
-            <input type=\"hidden\" name=\"cancel\" value=\"0\">
-            <input type=\"submit\" value=\"Non\">
-                </form>";
-                if (isset($_REQUEST['delete'])) {
-                    $newid = $line['id'];
-                    $bdd->exec('DELETE FROM rebirth WHERE id = \''.$line['id'].'\'');
-                    $bdd->query('UPDATE rebirth SET id=id - 1 WHERE id>'.$newid);
-                    header('Location: '.$_SERVER['HTTP_HOST']);
-                } elseif (isset($_REQUEST['cancel'])) {
-                    header('Location: '.$_SERVER['HTTP_HOST']);
-                }
-        }
-    }
 }
 
 function uploadCover() {
