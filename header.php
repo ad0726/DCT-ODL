f1e77edb0a0dd906f53933f074ac4c4aa1f47193
ad0726 <ad0726@hotmail.fr>
2018-07-23 22:24:30 +0200

    optimize login and logout

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php 
-session_start(); 
+session_start();
 include('function.php');
 ?>
 <!DOCTYPE html>
@@ -12,7 +12,7 @@ include('function.php');
         <link rel="stylesheet" href="assets/css/general.css?1.0.5">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="assets/js/script.js"></script>
+        <script type="text/javascript" src="assets/js/script.js?1.0.0"></script>
     <head>
     <body>
         <main>
