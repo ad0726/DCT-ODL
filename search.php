c0d2c2fcedcde4a52fac2b6cc37650cfbe5b18ce
Ady <ad0726@hotmail.fr>
2018-11-22 00:00:26 +0100

    [Feature] WIP: function search

---
+++ search.php
@@ -0,0 +1,10 @@
+<?php
+$period = str_replace(['.php', '/'], '', $_SERVER['REQUEST_URI']);
+echo "<div class='search'>
+    <form class='search-bar' action='results.php' method='post'>
+        <input class='prompt' type='text' name='search' placeholder='Reherche...'>
+        <input class='hide-input' type='text' name='period' value='".$period."'>
+        <i class='fas fa-search'></i>
+        <input type='submit' class='hide-input'>
+    </form>
+</div>";
