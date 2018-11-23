8ae6fa43d750bb5840a5ea1c05237c91cf99442d
aalmeras <aalmeras@vente-privee.com>
2018-11-23 15:42:45 +0100

    style.css and script.js versionning

--- header.php
+++ header.php
@@ -9,10 +9,10 @@ include('function.php');
         <link rel="icon" href="assets/img/icon_dct.png" type="image/png" sizes="16x16">
         <link rel="icon" href="assets/img/icon_dct.png" type="image/png" sizes="32x32">
         <link rel="stylesheet" href="assets/css/general.css?1.0.8">
-        <link rel="stylesheet" href="assets/css/style.css?1.3.9">
+        <link rel="stylesheet" href="assets/css/style.css?1.4.0">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="assets/js/script.js?2.1.0"></script>
+        <script type="text/javascript" src="assets/js/script.js?2.1.2"></script>
     <head>
     <body>
         <main>
