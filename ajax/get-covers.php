bd91f66da539cae12f0b110b6e9053ea1af53aec
ady <ad0726@hotmail.fr>
2019-04-28 01:35:15 +0200

    feat: get covers

---
+++ ajax/get-covers.php
@@ -0,0 +1,17 @@
+<?php
+$ROOT = "../";
+include($ROOT.'conf/conf.php');
+
+if (isset($_REQUEST['ids']) && isset($_REQUEST['era'])) {
+    $era     = $_REQUEST['era'];
+    $firstId = $_REQUEST['ids'][0];
+    $lastId  = end($_REQUEST['ids']);
+    $query   = $bdd->query("SELECT cover FROM odldc_$era WHERE id BETWEEN $firstId AND $lastId");
+
+    while ($cover = $query->fetch(PDO::FETCH_ASSOC)) {
+        $covers[] = $cover['cover'];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($covers);
+}
--- ajax/refresh-data.php
+++ ajax/refresh-data.php
@@ -2,7 +2,7 @@
 $ROOT = "../";
 include($ROOT.'conf/conf.php');
 
-if (($_REQUEST['formfilled'] == 42) && (!empty($_REQUEST['id'])) && (!empty($_REQUEST['name_era'])) && (!empty($_REQUEST['page']))) {
+if (($_REQUEST['formfilled'] == 42) && (!empty($_REQUEST['id'])) && (!empty($_REQUEST['name_era']))) {
     $line = $bdd->query('SELECT * FROM odldc_'.$_REQUEST['name_era'].' WHERE id = '.$_REQUEST['id'])->fetch(PDO::FETCH_ASSOC);
     $line['period'] = ($bdd->query('SELECT name FROM odldc_period WHERE id_period = "'.$line['id_period'].'"')->fetch(PDO::FETCH_ASSOC))['name'];
 
