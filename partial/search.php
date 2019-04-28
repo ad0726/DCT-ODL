a0675e4851dc9c28c874caf5d61d95fe0f399590
ady <ad0726@hotmail.fr>
2019-04-28 22:22:42 +0200

    fix: path for search input

--- partial/search.php
+++ partial/search.php
@@ -5,7 +5,7 @@ if (isset($_REQUEST['era']) && !empty($_REQUEST['era'])) {
     $era = "";
 }
 echo "<div class='search'>
-    <form class='search-bar' action='results.php' method='post'>
+    <form class='search-bar' action='/results.php' method='post'>
         <input class='prompt' type='text' name='search' placeholder='Reherche...' autocomplete='off'>
         <input class='hide-input era' type='text' name='era' value='$era'>
         <i class='fas fa-search'></i>
