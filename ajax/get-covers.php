7459d1c727ab1b4f7477a2370560cab9e9af2b99
ady <ad0726@hotmail.fr>
2019-06-24 13:32:29 +0200

    fix: order get covers

--- ajax/get-covers.php
+++ ajax/get-covers.php
@@ -6,7 +6,7 @@ if (isset($_REQUEST['ids']) && isset($_REQUEST['era'])) {
     $era     = $_REQUEST['era'];
     $firstId = $_REQUEST['ids'][0];
     $lastId  = end($_REQUEST['ids']);
-    $query   = $bdd->query("SELECT cover FROM odldc_$era WHERE id BETWEEN $firstId AND $lastId");
+    $query   = $bdd->query("SELECT cover FROM odldc_$era WHERE id BETWEEN $firstId AND $lastId ORDER BY id ASC");
 
     while ($cover = $query->fetch(PDO::FETCH_ASSOC)) {
         $covers[] = $cover['cover'];
